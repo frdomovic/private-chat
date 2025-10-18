b9ed0ed009b0ad10b2be75176fd73ded0701caf0
Fran <frandomovic@Frans-MacBook-Pro-2.local>
2025-10-18 22:33:15 +0200

    fix: wrapup

--- app/dev-dist/sw.js
+++ app/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-c6a197bf'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.0lio82i1918"
+    "revision": "0.hol5qh07ju8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
